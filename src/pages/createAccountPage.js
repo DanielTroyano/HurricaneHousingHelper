95fa2278fa626a3039200175c77dd4b18c5cee71
Arnoldo <arnoldovilches6@gmail.com>
2024-11-22 15:17:43 -0500

    Fixed problems with last that broke the register button

--- src/pages/createAccountPage.js
+++ src/pages/createAccountPage.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-
-// function createAccountPage() {
-//   return (
-//     <div className="createAccountPage">
-//       <h1>Create an Account</h1>
-//       <label htmlFor="username">Username:</label>
-//       <input type="text" id="username" name="username" />
-//       <br />
-
-//       <label htmlFor="password">Password:</label>
-//       <input type="password" id="password" name="password" />
-//       <br />
-
-//       <button>Create Account</button>
-//     </div>
-//   );
-// }
-
-// export default createAccountPage;
-
 import React, { useState } from 'react';
 import { firestore } from '../firebase'; // Import Firestore from your firebase.js file
 import { collection, addDoc } from 'firebase/firestore'; // Firestore functions
